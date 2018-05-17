36e45bd49cfc45afa96b5fdcddf1d38122346652
JustMatt <MatthewBertrandt@gmail.com>
2018-05-17 16:38:12 -0700

    Updated for 5-17

---
+++ Week3/Activities/Functions & Objects/objects.js
@@ -0,0 +1,7 @@
+var me = {
+    name: "Matt",
+    age: 21,
+    height: 71.5,
+    ssn: 000-00-0000,
+    
+};
---
+++ Week3/Activities/Java Variables/java.js
@@ -0,0 +1,8 @@
+var name = "jerma985";
+var strin = 'actuallyastringhaha';
+var notanumber = '12';
+var age = 32;
+var obejct = {};
+var array = [1, true, 3, "c:"];
+var bool = false;
+var zer0 = 0.283428374293499384;
